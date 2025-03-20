758f83545bd19a68d34c0ecc153dcaf05cccebf4
phiac <45037147+phiac@users.noreply.github.com>
2025-03-20 21:03:09 -0100

    Update settings.py

--- myvoice/settings.py
+++ myvoice/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = "django-insecure-y0&g8dvlox@d92kfqfl+y%ad2ct)go+*+$)a7h7c+gsqpq@^bf
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['phi-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['phi-production.up.railway.app'],
 
 
 
