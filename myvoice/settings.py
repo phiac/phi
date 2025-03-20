ee0f8bf3177b64207435df590d90103e880893d3
phiac <45037147+phiac@users.noreply.github.com>
2025-03-20 17:20:51 -0100

    Update settings.py

--- myvoice/settings.py
+++ myvoice/settings.py
@@ -42,7 +42,7 @@ LOGGING = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['phi-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['phi-production.up.railway.app', 'localhost', '127.0.0.1'],
 
 
 
