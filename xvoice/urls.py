706151c05a527ff41a2d7fdad9c694407c8a0570
phiac <45037147+phiac@users.noreply.github.com>
2025-03-21 10:11:57 -0100

    Update urls.py

--- xvoice/urls.py
+++ xvoice/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
     path('login/', lambda request: redirect('/accounts/login/')),  # Redirect /login to /accounts/login/
     path('accounts/login/', CustomLoginView.as_view(), name='login'),  # Add this line
-    path('register/', register, name='register'),
+    path('accounts/register/', views.register, name='register'),
     # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
